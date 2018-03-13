7862cd5bfaf7913af6ba2b1e17162d47b4e8c7ad
LoanaWitch <loanawitch@live.com>
2018-03-13 20:21:02 +0100

    date init

--- main.js
+++ main.js
@@ -2,5 +2,14 @@ var d = new Date();
 var ora = d.getHours();
 var minuti = d.getMinutes();
 var sec = d.getSeconds();
+var year = d.getFullYear();
+var month = d.getMonth();
+var day = d.getDate();
+var dayofweek = d.getDay();
 
-document.write ( d )
+document.getElementById('ora').innerHTML = ora
+document.getElementById('min').innerHTML = minuti
+document.getElementById('sec').innerHTML = sec
+document.getElementById('day').innerHTML = day
+document.getElementById('mese').innerHTML = month
+document.getElementById('anno').innerHTML = year
